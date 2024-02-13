0dea45362680bad30c7a0c7b593b67db8de50fb3
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-02-13 14:56:51 +0200

    Create a new template 3-hbnb.html (based on 2-hbnb.html) and update it:
        Import the JavaScript static/scripts/3-hbnb.js in the <head> tag (instead of 2-hbnb.js)
        Remove the entire Jinja section of displaying all places (all article tags)

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,93 @@
+/**
+ * Listen for changes on each input checkbox tag
+ */
+document.addEventListener('DOMContentLoaded', function () {
+	$(function () {
+		const amenitiesDict = [];
+		$('input[type="checkbox"]').change(function () {
+			const amenityID = $(this).data('id');
+			const amenityName = $(this).data('name');
+
+			if ($(this).is(':checked')) {
+				amenitiesDict.push({ id: amenityID, name: amenityName });
+			} else {
+				let indexToRemove = -1;
+				for (let i = 0; i < amenitiesDict.length; i++) {
+					if (amenitiesDict[i].id === amenityID) {
+						indexToRemove = i;
+						break;
+					}
+				}
+
+				// Remove the amenity from the array if found
+				if (indexToRemove !== -1) {
+					amenitiesDict.splice(indexToRemove, 1);
+				}
+			}
+			const amenitiesList = amenitiesDict.map(function (amenity) {
+				return amenity.name;
+			}).join(', ');
+			$('.amenities h4').text(amenitiesList);
+		});
+
+		$.ajax({
+			type: 'GET',
+			url: 'http://0.0.0.0:5001/api/v1/status/',
+			data: 'data',
+			dataType: 'json',
+			success: function (data) {
+				if (data.status === 'OK') {
+					$('div#api_status').addClass('available');
+				} else {
+					$('div#api_status').removeClass('available');
+				}
+			}
+		});
+
+		$.ajax({
+			type: "POST",
+			url: "http://0.0.0.0:5001/api/v1/places_search/",
+			contentType: "application/json",
+			data: JSON.stringify({}),
+			dataType: "json",
+			success: function (response) {
+
+				$.each(response, function (i, place) {
+
+					$('section.places').append('<article>' +
+						'<div  class="title_box">'
+						+
+						'<h2>' + place.name + '</h2>'
+						+
+						'<div class="price_by_night">$' + place.price_by_night + '</div>'
+						+
+						'</div>'
+						+
+						'<div class="information">'
+						+
+						'<div class="max_guest">' + place.max_guest + (place.max_guest != 1 ? " Guests" : " Guest") + '</div>'
+						+
+						'<div class="number_rooms">' + place.number_rooms + ' Bedroom' + (place.number_rooms != 1 ? "s" : "") + '</div>'
+						+
+						'<div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom' + (place.number_bathrooms != 1 ? "s" : "") + '</div>'
+						+
+						'</div>'
+						+
+						'<div class="description">'
+						+
+						place.description + '</div>'
+						+
+						'</article>');
+					console.log(response);
+				});
+
+
+			},
+			error: function (xhr, status, error) {
+				console.error(xhr.responseText);
+			}
+		});
+
+
+	});
+});
